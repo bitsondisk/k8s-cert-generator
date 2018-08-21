26ee70438b412963f87ef424beff9da2b272b059
Kevin Burke <kevin+freenome@burke.services>
2018-08-21 14:36:22 -0700

    Add Travis CI configuration

---
+++ scripts/push-to-gcr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Decrypt GCR deploy key
+openssl aes-256-cbc -K $encrypted_f995266e8b6f_key -iv $encrypted_f995266e8b6f_iv -in gcr-deploy.json.enc -out gcr-deploy.json -d
+
+# Make sure we've got a gcloud client ...
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]
+then
+  rm -rf $HOME/google-cloud-sdk
+  curl https://sdk.cloud.google.com | bash
+fi
+
+# Add gcloud to our PATH
+source ~/google-cloud-sdk/path.bash.inc
+
+# Activate travis-container-builder service account
+gcloud auth activate-service-account travis-container-builder@freenome-build.iam.gserviceaccount.com --key-file gcr-deploy.json
+make docker-prod
+gcloud docker -- push gcr.io/freenome-build/${SERVICE_NAME}:${TRAVIS_COMMIT}
